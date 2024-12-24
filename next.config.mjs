6a6e2a167e68fa9699a916b66c0ed03242fee3e3
kajian <929932952@qq.com>
2024-12-24 16:58:39 +0800

    feat：修改请求配置

--- next.config.mjs
+++ next.config.mjs
@@ -34,6 +34,23 @@ const nextConfig = {
     }
     return config;
   },
+  async headers () {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains'
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+        ],
+      },
+    ]
+  },
 };
 
 export default withContentlayer(nextConfig);
