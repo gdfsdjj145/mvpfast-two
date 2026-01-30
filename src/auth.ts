13e5362a29d9061299fe706fceac1e352b59d859
kajian <929932952@qq.com>
2026-01-30 17:54:57 +0800

    feat: auth问题

--- src/auth.ts
+++ src/auth.ts
@@ -161,17 +161,6 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     maxAge: 2 * 60 * 60, // 2小时
     updateAge: 60 * 60,
   },
-  cookies: {
-    sessionToken: {
-      name: `next-auth.session-token`,
-      options: {
-        httpOnly: true,
-        sameSite: 'lax',
-        path: '/',
-        secure: process.env.NODE_ENV === 'production',
-      },
-    },
-  },
   callbacks: {
     async session({ token, session }) {
       if (session.user) {
