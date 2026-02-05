95e83dde22b3425771f87cc80f5ee4b7393166f5
kajian <929932952@qq.com>
2026-02-05 20:54:14 +0800

    init

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 import { createMDX } from 'fumadocs-mdx/next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
-const withNextIntl = createNextIntlPlugin();
+const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 
 const withMDX = createMDX({});
 
