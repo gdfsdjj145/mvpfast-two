a7f04d4d383f29d5549171827f0bdaf3aea8d9ab
kajian <929932952@qq.com>
2026-02-06 10:03:08 +0800

    feat: 合并

--- src/app/(main)/[local]/page.tsx
+++ src/app/(main)/[local]/page.tsx
@@ -8,18 +8,9 @@ const BrandComponent = dynamic(() => import('@/components/landingpage/BrandCompo
 const AIShowcaseComponent = dynamic(() => import('@/components/landingpage/AIShowcaseComponent'));
 const AdminComponent = dynamic(() => import('@/components/landingpage/AdminComponent'));
 const FeatureComponent = dynamic(() => import('@/components/landingpage/FeatureComponent'));
-<<<<<<< HEAD
 const CaseComponent = dynamic(() => import('@/components/landingpage/CaseComponent'));
 const PriceComponent = dynamic(() => import('@/components/landingpage/PriceComponent'));
 const FaqComponents = dynamic(() => import('@/components/landingpage/FaqComponents'));
-=======
-const FeaturesCard = dynamic(() => import('@/components/landingpage/FeaturesCardComponent'));
-const FeaturesGrid = dynamic(() => import('@/components/landingpage/FeaturesGridComponent'));
-const CaseComponent = dynamic(() => import('@/components/landingpage/CaseComponent'));
-const PriceComponent = dynamic(() => import('@/components/landingpage/PriceComponent'));
-const FaqComponents = dynamic(() => import('@/components/landingpage/FaqComponents'));
-const FaqListComponent = dynamic(() => import('@/components/landingpage/FaqListComponent'));
->>>>>>> upstream/feature/2.0
 
 export default function Home() {
   return (
@@ -31,18 +22,9 @@ export default function Home() {
         <AIShowcaseComponent />
         <AdminComponent />
         <FeatureComponent />
-<<<<<<< HEAD
         <CaseComponent />
         <PriceComponent />
         <FaqComponents />
-=======
-        <FeaturesCard />
-        <FeaturesGrid />
-        <CaseComponent />
-        <PriceComponent />
-        <FaqComponents />
-        <FaqListComponent />
->>>>>>> upstream/feature/2.0
       </main>
       <Footer />
     </div>
